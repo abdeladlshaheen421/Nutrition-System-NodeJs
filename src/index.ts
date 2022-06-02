ae1e268c10c8256f462106202341d7aa9a0079b2
abdeladlshaheen <abdeladlshaheen@gmail.com>
2022-06-02 13:04:45 +0200

    export app

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
-import router from './routers/router';
 
 dotenv.config();
 const { SERVER_PORT } = process.env;
@@ -28,10 +27,11 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
+// home page
 
-// handle all routess
-app.use(router);
-
+app.get('/', (req: express.Request, res: express.Response): void => {
+  res.status(200).json({welcomeMessage:'Welcome To Our Nutrition System'});
+});
 // Not Found Handler
 app.use((req: Request, res: Response, next: NextFunction): Response => {
   return res.status(404).json({ error: 'Route is Not Found' });
@@ -48,3 +48,5 @@ app.use(
     return res.status(500).json({ error: error + ' ' });
   }
 );
+
+export default app
