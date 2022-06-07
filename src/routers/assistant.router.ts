b09288e22ce54784cf71cf2d82028c0b39614f84
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-06-07 15:15:42 +0200

    edit assistant in routers for roles

--- src/routers/assistant.router.ts
+++ src/routers/assistant.router.ts
@@ -1,23 +1,20 @@
 import { NextFunction, Request, Response, Application } from 'express';
-import { AssistantController } from '../controllers/assistant.controller';
-import {validateCreation, validateUpdate}  from './../middlewares/assistant.middleware';
-import { validationResult } from 'express-validator';
-import {isValidIdParam } from '../middlewares/clinic.middleware';
+import {
+  AssistantController,
+  AssistantType,
+} from '../controllers/assistant.controller';
+import {
+  validateCreation,
+  validateUpdate,
+} from './../middlewares/assistant.middleware';
+import { isValidIdParam } from '../middlewares/clinic.middleware';
+import { validatePassword } from './../middlewares/client.middleware';
+import { validate } from './client.router';
+import { verifyAuthToken } from './client.router';
 
 const assistantInstance = new AssistantController();
 
-function validate(req: Request): void {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error();
-    error.message = errors
-      .array()
-      .reduce((current, object) => current + object.msg + ' ', '');
-    throw error;
-  }
-}
-
-const create = async (
+const createAssistant = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -37,7 +34,10 @@ const login = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    const assistant = await assistantInstance.login(req.body.email, req.body.password);
+    const assistant = await assistantInstance.login(
+      req.body.email,
+      req.body.password
+    );
     if (assistant) {
       const token = await assistantInstance.generateJWT(assistant);
       res.status(200).json({ token });
@@ -49,68 +49,182 @@ const login = async (
   }
 };
 
-const index = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      validate(req);
+const getAllAssistants = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    validate(req);
+    if (
+      res.locals.authUser.role === 'admin' ||
+      res.locals.authUser.role === 'clinicAdmin'
+    ) {
       const assistants = await assistantInstance.index();
       res.status(200).json({ assistants });
-    } catch (error) {
-      next(error);
+    } else {
+      res
+        .status(401)
+        .json({ message: 'You are Unauthorized to do this action' });
     }
-  };
-  
-  const show = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      validate(req);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const showAssistant = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    validate(req);
+    if (
+      res.locals.authUser.role === 'admin' ||
+      res.locals.authUser.role === 'clinicAdmin' ||
+      (res.locals.authUser.role === 'assistant' && res.locals.authUser.id === req.params.id)
+    ) {
       const assistant = await assistantInstance.show(req.params.id);
       res.status(200).json({ assistant });
-    } catch (error) {
-      next(error)
+    } else {
+      res
+        .status(401)
+        .json({ message: 'You are Unauthorized to do this action' });
     }
-  };
-  
-  const update = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      validate(req);
-      delete req.body.password;
-      const assistant = await assistantInstance.update(
-        req.params.id,
-        req.body
-      );
-      res.status(200).json({ assistant });
-    } catch (error) {
-      next(error)
-    }
-  };
-  
-  const remove = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      validate(req);
-      const assistant = await assistantInstance.delete(req.params.id);
-      res.status(200).json({ assistant });
-    } catch (error) {
-      next(error)
-    }
-  };
+  } catch (error) {
+    next(error);
+  }
+};
 
-  const showAssistantsClinic = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      validate(req);
-      const assistantsClinic = await assistantInstance.showAssistantsClinic(req.params.id);
-      res.status(200).json({ assistantsClinic });
-    } catch (error) {
-      next(error)
+const updateAssistant = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    validate(req);
+    if (
+      res.locals.authUser.role === 'admin' ||
+      res.locals.authUser.role === 'clinicAdmin'
+    ) {
+      delete req.body.password;
+      const assistant = await assistantInstance.update(req.params.id, req.body);
+      res.status(200).json({ assistant });
+    } else {
+      res
+        .status(401)
+        .json({ message: 'You are Unauthorized to do this action' });
     }
-  };
+  } catch (error) {
+    next(error);
+  }
+};
+
+const removeAssistant = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    validate(req);
+    if (
+      res.locals.authUser.role === 'admin' ||
+      res.locals.authUser.role === 'clinicAdmin'
+    ) {
+      await assistantInstance.delete(req.params.id);
+      res.status(200).json({ message: 'Assistant deleted successfully' });
+    } else {
+      res
+        .status(401)
+        .json({ message: 'You are Unauthorized to do this action' });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const showAssistantsClinic = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    validate(req);
+    if (
+      res.locals.authUser.role === 'admin' ||
+      res.locals.authUser.role === 'clinicAdmin'
+    ) {
+      const assistantsClinic = await assistantInstance.showAssistantsClinic(
+        req.params.id
+      );
+      res.status(200).json({ assistantsClinic });
+    } else {
+      res
+        .status(401)
+        .json({ message: 'You are Unauthorized to do this action' });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+const updatePassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    validate(req);
+    const oldPassword = req.body.oldPassword;
+    const newPassword = req.body.newPassword;
+    const correctPassword = await assistantInstance.findByPassword(
+      req.params.id,
+      oldPassword
+    );
+    if (correctPassword) {
+      const client: AssistantType = await assistantInstance.updatePassword(
+        req.params.id,
+        newPassword
+      );
+      res.status(200).json({ client });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const assistantRouter = (app: Application) => {
-    app.get('/assistants', index);
-    app.get('/assistants/:id',isValidIdParam, show);
-    app.get('/assistants/clinic/:id',isValidIdParam, showAssistantsClinic);
-    app.post('/assistants/create', validateCreation, create);
-    app.post('/assistants/login', login);
-    app.patch('/assistants/update/:id',isValidIdParam, validateUpdate, update);
-    app.delete('/assistants/:id',isValidIdParam, remove);
-  };
+  app.get('/assistants', verifyAuthToken, getAllAssistants);
+  app.get('/assistants/:id', isValidIdParam, verifyAuthToken, showAssistant);
+  app.get(
+    '/assistants/clinic/:id',
+    isValidIdParam,
+    verifyAuthToken,
+    showAssistantsClinic
+  );
+  app.post('/assistants/create', validateCreation, createAssistant);
+  app.post('/assistants/login', login);
+  app.patch(
+    '/assistants/update/:id',
+    isValidIdParam,
+    validateUpdate,
+    verifyAuthToken,
+    updateAssistant
+  );
+  app.patch(
+    '/assistants/:id/password',
+    isValidIdParam,
+    validatePassword,
+    verifyAuthToken,
+    updatePassword
+  );
+  app.delete(
+    '/assistants/:id',
+    isValidIdParam,
+    verifyAuthToken,
+    removeAssistant
+  );
+};
 
 export default assistantRouter;
